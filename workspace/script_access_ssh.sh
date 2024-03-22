a6c2fd0aa66d43fecebc824fbd69f6d15df9ef8c
paul gualambo giraldo <paul.gualambo@gmail.com>
2024-03-21 21:52:14 -0500

    add var for ssh

--- workspace/script_access_ssh.sh
+++ workspace/script_access_ssh.sh
@@ -15,6 +15,18 @@ HOST_APP="192.168.18.141"
 HOST_INFRA="192.168.18.142"
 HOST_DEPLOY="192.168.18.143"
 
-ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${HOST_APP}
-ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${HOST_INFRA}
-ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${HOST_DEPLOY}
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${WORKSPACE}_${APP_HOST}
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${WORKSPACE}_${INFRA_HOST}
+ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER_T}@${WORKSPACE}_${DEPLOY_HOST}
+
+ssh-copy-id -i ~/.ssh/id_ed25519.pub ${USER_T}@${WORKSPACE}_${APP_HOST}
+ssh-copy-id -i ~/.ssh/id_ed25519.pub ${USER_T}@${WORKSPACE}_${INFRA_HOST}
+ssh-copy-id -i ~/.ssh/id_ed25519.pub ${USER_T}@${WORKSPACE}_${DEPLOY_HOST}
+
+
+
+#PAUL-LAPTOP windows
+setx WORKSPACE SANDBOX
+setx APP_HOST 192.168.202.124
+setx INFRA_HOST 192.168.202.125
+setx DEPLOY_HOST 192.168.202.126
